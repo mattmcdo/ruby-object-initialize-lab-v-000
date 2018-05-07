e5be35221bb77f8f3be297b3465cf10adc1a636b
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-05-07 00:01:17 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,24 +1,6 @@
 class Dog
-  def initialize(name, breed = "Mutt")
-    @name = name
-    @breed = breed
-  end
-
-  def name= (name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
-
-  def breed= (breed)
-    @breed = breed
-    end
-
-  def breed
-    @breed
-  end
+def initialize(name, breed = "Mutt")
+@name = name
+@breed = breed
 end
-
-fido = Dog.new("Fido", @breed)
+end
